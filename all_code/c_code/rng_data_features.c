131b13e5627aae71a55d345a41ade05abea2dfd2
Avalon8D <pedroaschmidt@gmail.com>
2020-02-01 17:55:02 -0300

    Removes unused C code.

--- all_code/c_code/rng_data_features.c
+++
@@ -1,9 +0,0 @@
-// implement random fourier features for data_set
-// sample im many ways
-// i.e. from fourier integral and from other spd kernel transforms
-// from sample sums of kernel from a given distribution
-
-// think about devising sampling tree for sampling kernel combinations
-// (long term)
-
-// proclus over randomized feature maps
